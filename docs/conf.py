20980370d37b351f73f5a8ad3f8894401c9cc5c6
peter <petrel.harp@gmail.com>
2026-09-03 16:42:24 -0700

    switch to pyproject.toml and uv?

--- docs/conf.py
+++ docs/conf.py
@@ -219,7 +219,7 @@ epub_exclude_files = ["search.html"]
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/3", None),
+    # "python": ("https://docs.python.org/3", None),
     "tskit": ("https://tskit.dev/tskit/docs/stable", None),
     "msprime": ("https://tskit.dev/msprime/docs/stable", None),
     "pyslim": ("https://tskit.dev/pyslim/docs/stable", None),
